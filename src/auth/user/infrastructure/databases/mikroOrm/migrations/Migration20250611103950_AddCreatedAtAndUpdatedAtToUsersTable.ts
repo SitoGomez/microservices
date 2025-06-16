58ecc787fc439cb2fee8a1310a0988349f573749
JoseGomezSevillano <jose.gomez@qualifyze.com>
2025-06-16 20:12:43 +0200

    feat: add migration to support missing colums

---
+++ src/auth/user/infrastructure/databases/mikroOrm/migrations/Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable.ts
@@ -0,0 +1,17 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable extends Migration {
+  public override up(): void {
+    this.addSql(
+      `ALTER TABLE users
+        ADD COLUMN IF NOT EXISTS created_at TIMESTAMPTZ NOT NULL,
+        ADD COLUMN IF NOT EXISTS modified_at TIMESTAMPTZ NOT NULL;`,
+    );
+  }
+
+  public override down(): void {
+    this.addSql(`ALTER TABLE users
+      DROP COLUMN IF EXISTS created_at,
+      DROP COLUMN IF EXISTS modified_at;`);
+  }
+}
--- src/auth/user/infrastructure/databases/mikroOrm/migrations/index.ts
+++ src/auth/user/infrastructure/databases/mikroOrm/migrations/index.ts
@@ -1,6 +1,7 @@
 import { Migration20250611103928_CreateUsersTable } from './Migration20250611103928_CreateUsersTable';
 import { Migration20250611103929_MakeEmailUniqueUsersTable } from './Migration20250611103929_MakeEmailUniqueUsersTable';
 import { Migration20250611103940_RemoveFullnameColumnFromUsersTable } from './Migration20250611103940_RemoveFullnameColumnFromUsersTable';
+import { Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable } from './Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable';
 
 export const authMigrations = [
   {
@@ -14,5 +15,9 @@ export const authMigrations = [
   {
     name: Migration20250611103940_RemoveFullnameColumnFromUsersTable.name,
     class: Migration20250611103940_RemoveFullnameColumnFromUsersTable,
+  },
+  {
+    name: Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable.name,
+    class: Migration20250611103950_AddCreatedAtAndUpdatedAtToUsersTable,
   },
 ];
