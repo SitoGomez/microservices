753ceb1675c3b0d47e688e5dcdf1c657f309e771
JoseGomezSevillano <jose.gomez@qualifyze.com>
2025-06-16 20:12:17 +0200

    feat: extend mapper

--- src/auth/user/infrastructure/databases/mikroOrm/MikroOrmUserMapper.ts
+++ src/auth/user/infrastructure/databases/mikroOrm/MikroOrmUserMapper.ts
@@ -1,18 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
+import {
+  DOMAIN_EVENT_MANAGER,
+  IDomainEventManager,
+} from '../../../../../shared/domainEvent/domain/IDomainEventManager';
 import { User } from '../../../domain/User';
 
 import { UserEntity } from './entities/User.entity';
 
 @Injectable()
 export class MikroOrmUserMapper {
+  public constructor(
+    @Inject(DOMAIN_EVENT_MANAGER)
+    private readonly domainEventManager: IDomainEventManager,
+  ) {}
+
   public fromDomain(user: User): UserEntity {
     const userEntity = new UserEntity(
       user.getId(),
       user.getEmail(),
       user.getPassword(),
+      user.getCreatedAt(),
+      user.getUpdatedAt(),
     );
 
     return userEntity;
+  }
+
+  public toDomain(userEntity: UserEntity): User {
+    return User.fromPrimitives(
+      this.domainEventManager,
+      userEntity.getUserId(),
+      userEntity.getEmail(),
+      userEntity.getPassword(),
+      userEntity.getCreatedAt(),
+      userEntity.getUpdatedAt(),
+    );
   }
 }
